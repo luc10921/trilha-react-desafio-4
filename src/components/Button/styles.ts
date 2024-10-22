d23f2f3ae070aeeec815e9cacc1234502f6898c1
Lucas Kenji Uezu <kenjiuezu3@gmail.com>
2024-10-22 18:32:57 -0300

    Disable button if values are not valid

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -14,4 +14,10 @@ export const ButtonContainer = styled.button`
         opacity: 0.6;
         cursor:pointer;
     }
+
+    &:disabled {
+        background-color: #81259D;
+        opacity: 0.6;
+        cursor:not-allowed;
+    }
 `
